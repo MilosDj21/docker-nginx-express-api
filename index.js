b5286337d71b225342f19f928ee338f16da2dec0
Milos <m.djuka@hotmail.com>
2024-03-24 19:57:44 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_DB_NAME } = require("./config/config");
 const userRoutes = require("./routes/user");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+// Since nginx proxies requests, and sets some custom headers, this is required to be able to access those properties
+app.enable("trust proxy");
 
 app.use("/api/v1/users", userRoutes);
 
